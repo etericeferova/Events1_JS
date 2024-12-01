8f217ee8ab45c3019f94c727b8c1090f6ea6252c
etericeferova <147832143+etericeferova@users.noreply.github.com>
2024-12-01 22:47:14 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+const lights = document.querySelectorAll(".light");
+let currentLight = 0;
+
+function updateTrafficLight() {
+  lights.forEach((light, index) => {
+    light.classList.toggle("active", index === currentLight);
+  });
+}
+
+function nextLight() {
+  currentLight = (currentLight + 1) % lights.length;
+  updateTrafficLight();
+}
+
+updateTrafficLight();
