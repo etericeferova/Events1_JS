d7b2d50ae3158f9e36bd367170be77c5b7544c1d
etericeferova <147832143+etericeferova@users.noreply.github.com>
2024-12-01 23:05:13 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+const bookList = document.getElementById("bookList");
+let selectedBook = null;
+
+bookList.addEventListener("click", (event) => {
+  const clickedBook = event.target;
+
+  if (clickedBook.tagName === "LI") {
+    if (selectedBook) {
+      selectedBook.style.backgroundColor = "";
+    }
+    clickedBook.style.backgroundColor = "orange";
+    selectedBook = clickedBook;
+  }
+});
